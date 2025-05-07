cba23a81f251c0264fd1d2bba08fba5bde53dc2e
Cristian07765 <arenacristian009@gmail.com>
2025-05-07 09:37:05 +0200

    SI

--- JS/Footer.js
+++ JS/Footer.js
@@ -3,9 +3,9 @@ function Footer() {
         <footer>
                 <br>
                 I contenuti del presente Sito - comprensivi di dati, notizie , informazioni, immagini, grafici, marchi e nomi a dominio - sono di propriet\u00E0 di Avventure Mediterranee S.p.a
-                <br> Avventure Mediterranee S.p.a &copy; 2024
+                <br> Avventure Mediterranee S.p.a &trade; 2024
                 <br><br> Corso Italia, 20 - 89900 Vibo Valentia (VV)
-                <br> Tel: +39 0963.1234567 | P.IVA 09123456789
+                <br> Tel: +39 123456789 | P.IVA: []
             </p>
         </footer>
     `;
